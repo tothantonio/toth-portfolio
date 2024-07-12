b57ab23bc885dfa3a723732fa06baab0981fba83
Antonio <tothantonio000@gmail.com>
2024-07-12 18:11:21 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Page() {
         Toth Antonio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a passionate engineering student with a keen interest in web and mobile development. 
+I thrive on solving complex problems and creating innovative solutions. Currently studying 
+Computer Science at TUCN.  
+Let's build something amazing together!😊🚀
+`}
       </p>
       <div className="my-8">
         <BlogPosts />
