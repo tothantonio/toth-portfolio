7247e21a7e7f10d38d03a35adadc7650b2a4403a
Antonio <tothantonio000@gmail.com>
2024-07-12 18:54:12 +0300

    q

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Toth Antonio',
+    template: '%s | Toth Antonio',
   },
   description: 'This is my portfolio.',
   openGraph: {
