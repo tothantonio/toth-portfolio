231a3da1afc5cf84cfa7a9b7d8d299cc8dfef602
Toth Antonio <tothantonio000@gmail.com>
2025-06-28 19:35:52 +0300

    .

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://docs.google.com/document/d/1I4t6pakxcb5ReFQzdeNPA3_uFe01W-nF3W9T09GL3P0/edit#heading=h.fk0meevqned9"
+            //href="https://docs.google.com/document/d/1I4t6pakxcb5ReFQzdeNPA3_uFe01W-nF3W9T09GL3P0/edit#heading=h.fk0meevqned9"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">resume</p>
