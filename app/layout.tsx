d70f0a23e06e6eba343e525ae65e4fe6e262e465
Toth Antonio <tothantonio000@gmail.com>
2026-07-08 12:08:00 +0300

    update project dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './global.css'
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
+// Geist font imports removed (not available in dependencies)
 import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
@@ -50,11 +49,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
-        GeistSans.variable,
-        GeistMono.variable
-      )}
+        className={cx('text-black bg-white dark:text-white dark:bg-black')}
     >
       
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
