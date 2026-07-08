fa30bf459494ddaa1359566343387953d6040f8a
Toth Antonio <tothantonio000@gmail.com>
2026-07-08 13:25:31 +0300

    add linkedin

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://www.linkedin.com/in/antonio-toth-b9542b317/"
+            href="http://www.linkedin.com/in/antonio-roberto-toth-b9542b317"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">linkedin</p>
